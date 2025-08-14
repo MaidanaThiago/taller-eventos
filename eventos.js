0eb887141f5561a71ef4f46661b53fcebd4bbd28
MaidanaThiago <maidana15t@gmail.com>
2025-08-14 15:37:30 -0300

    Solución al problema de propagación de eventos

--- eventos.js
+++ eventos.js
@@ -1,5 +1,13 @@
 const miDiv = document.getElementById("miDiv");
+const miBoton = miDiv.querySelector("button");
 
-miDiv.addEventListener("click",function(){
+
+miBoton.addEventListener("click", function(event) {
+    // Detiene la propagación del evento para que no llegue al div
+    event.stopPropagation();
+    alert("¡Hola! Soy el botón");
+});
+
+miDiv.addEventListener("click", function() {
     alert("¡Hola! Soy el div");
-})
+});
